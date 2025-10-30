802d16804b3e4ce5f7c251f2a1e7936134a53e51
christianCarrion72 <christianco720@gmail.com>
2025-10-30 16:21:26 -0400

    nuevo estado

--- src/app/pages/citas/cita-dia.component.ts
+++ src/app/pages/citas/cita-dia.component.ts
@@ -297,6 +297,7 @@ export class CitaDiaComponent implements OnInit {
   getColorForState(estado: string): string {
     switch(estado) {
       case 'programada': return 'bg-blue-500';
+      case 'confirmada': return 'bg-yellow-500';  
       case 'completada': return 'bg-green-500';
       case 'cancelada': return 'bg-red-500';
       default: return 'bg-gray-500';
